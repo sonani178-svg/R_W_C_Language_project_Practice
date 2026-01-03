84bfd371ee9a3533b62fc250268b82c4db516dd4
sonani178-svg <sonani178@gmail.com>
2026-01-03 10:07:03 +0530

    addsub

---
+++ .vscode/arithmetic/sum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+int a=5 , b=10;
+printf("sum of : %d\n", a+b);
+printf("subtra of : %d\n", a-b);
+printf("multi of : %d\n", a*b);
+printf("division of : %d\n", a/b);
+printf("modulas of : %d\n", a/b);
+    return 0;
+}
--- AP/main.c
+++ AP/main.c
@@ -2,6 +2,14 @@
 
 int main() {
 printf ("hello world");
+
+ int n=10;
+    scanf("%d", &n);
+    if(n >=0)
+
+    printf("positive");
+    else
+    printf("Negative");
     return 0;
 
 }
