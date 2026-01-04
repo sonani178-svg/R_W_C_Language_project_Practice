684639005225c11f6aef4869bb54234d9681b02c
sonani178-svg <sonani178@gmail.com>
2026-01-04 13:43:43 +0530

    area of rect

--- arithmetic/rectangle.c
+++ arithmetic/rectangle.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 int main() {
-    int l = 20 , w = 10;
-    printf("area of rectangle : %d", l*w);
+    int  area , length , width;
+    float l = 5;
+    float w = 8.7;
+
+    scanf("%f%f", &area, &length ,&width);
     
-    
+ printf("area of rectangle =  %f", l * w);
+      
     return 0;
 
 }
