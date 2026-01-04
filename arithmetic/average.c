6450c3b508abd1f759855ae57d5a4c842da2dc7c
sonani178-svg <sonani178@gmail.com>
2026-01-04 23:03:08 +0530

    aver

---
+++ arithmetic/average.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+ 
+    float a = 8.5, b = 8.8, c = 8.2;
+    float average;
+    average = (a + b + c) / 3;
+    printf("average : %f%f%f", average);
+    
+    return 0;
+}
