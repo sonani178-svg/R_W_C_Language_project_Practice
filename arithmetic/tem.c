762d364c2a9b48cd703523d50f864cfe9b5ddb74
sonani178-svg <sonani178@gmail.com>
2026-01-04 22:42:09 +0530

    temprature

---
+++ arithmetic/tem.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    float celsius =20.68;
+    float farhenheit;
+ 
+    farhenheit = (celsius * 9/5) +32;
+    
+    printf("farhenheit : %f", farhenheit);
+
+    return 0;
+}
