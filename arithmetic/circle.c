ee8b523174109c2e58e053e2c43496c46623d0e6
sonani178-svg <sonani178@gmail.com>
2026-01-04 23:12:47 +0530

    circ

--- arithmetic/circle.c
+++ arithmetic/circle.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#define PI = 3.142
 int main(){
-  int area , PI , r;
+  float area;
   float PI = 3.142;
   float R = 5;
   
-  scanf("%f%f", &area ,&PI, &r);
-  printf("area of circle = %f", PI * r * r);
+  scanf("%f%f", &area ,&PI, &R);
+  printf("area of circle = %f", PI * R * R);
     
     return 0;
 }
