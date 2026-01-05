36f772d670c04e67d9b2ea0e111c0c97693dc6ad
sonani178-svg <sonani178@gmail.com>
2026-01-05 16:27:31 +0530

    relation

---
+++ relational/than.c
@@ -0,0 +1,21 @@
+//Relational operators
+
+#include <stdio.h>
+int main(){
+
+int a = 10;
+int b = 30;
+
+if (a < b){
+    printf("a is less than b");
+}
+else if (a > b){
+    printf("a is greater than b");
+}
+else 
+    printf("a and b are same");
+
+{}
+   return 0;
+ } /* code */
+
