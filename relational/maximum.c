61bd3a06b719a53cb8b3e5ab4a7c98afb37c75c9
sonani178-svg <sonani178@gmail.com>
2026-01-05 18:04:20 +0530

    two

---
+++ relational/maximum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+ int a = 10;
+ int b = 20;
+ if(a > b){
+    printf("a is maximum number");
+ }
+
+    else if(a < b){
+        printf("b is maximum number");
+
+    }
+ 
+    return 0;
+}
