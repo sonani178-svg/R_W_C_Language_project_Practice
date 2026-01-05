df3426692eb90e2244c1a2076c6d133fec8c54f0
sonani178-svg <sonani178@gmail.com>
2026-01-05 22:43:07 +0530

    fail

---
+++ relational/marks.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main (){
+int marks = 25;
+
+if(marks >= 35)
+{
+    printf("student is pass\n");
+}   
+else 
+{
+    printf("student is fail\n");
+}
+        
+    return 0;
+}
