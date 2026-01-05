dfdb02e0e6a857a726f39d2f988fb18df0ba1e8e
sonani178-svg <sonani178@gmail.com>
2026-01-05 08:18:17 +0530

    cube

---
+++ arithmetic/square.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+int n = 4;
+printf("Square :%d\n", n*n);
+printf("cube :%d", n*n*n);
+    return 0;
+}
---
+++ arithmetic/swap.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int a = 5;
+    int b 10;
+ int = swap no;
+    // using swap airthmetic operator
+
+    printf("swap num : %d", a b);
+    
+    
+    
+    return 0;
+}
