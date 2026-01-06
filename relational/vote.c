b58088f728d67af52eb10fdd8efcb86568f5cfcc
sonani178-svg <sonani178@gmail.com>
2026-01-06 06:15:19 +0530

    eligible

---
+++ relational/vote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+int age = 14;
+if (age >= 18)
+{
+    printf("person is eligible for vote");
+}
+else 
+{
+printf("person is not eligible for vote ");
+}
+
+    return 0;
+}
