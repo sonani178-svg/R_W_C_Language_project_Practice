62ec7a43adc45157966fbe53e06b3ae88b43ed2f
sonani178-svg <sonani178@gmail.com>
2026-01-06 23:15:34 +0530

    logical

---
+++ Logical operator/lies.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+int a;
+
+printf("enter numbers");
+scanf ("%d", &a);
+
+if(1<10 && 1>10){
+
+    printf("%d lies between 1 to 10", a);
+}else{
+    printf("%d does not lies between 1 to  10", a);
+}
+
+
+    return 0;
+}
