04d562ad8186991e5add9507d99dcbe9f75488e8
sonani178-svg <sonani178@gmail.com>
2026-01-06 06:51:26 +0530

    equal

---
+++ relational/numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+int E = 30;
+int F = 78;
+if(E!=F)
+{
+    printf("both number are not equal");
+
+}
+else if (E=F)
+printf("both number are equal");
+
+    return 0;
+}
