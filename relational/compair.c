8be115ef424c1664fd45c9cc81824c1ff284b372
sonani178-svg <sonani178@gmail.com>
2026-01-06 06:45:26 +0530

    marks

---
+++ relational/compair.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+int marks1 = 24;
+int marks2 = 34;
+
+if (marks1 > marks2)
+{
+    printf("student1 scored is higher\n");
+}
+    else if (marks1 < marks2)
+    {
+        printf ("studrnt2 scored is higher\n");
+    }
+else
+{
+    printf("both students scored are equal\n");
+}
+
+    return 0;
+}
